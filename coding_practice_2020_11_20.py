3d92c0dd14841ed71a32bca8776bf1c4f827328e
NSath05 <74628036+NSath05@users.noreply.github.com>
2020-11-20 13:08:58 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,1 +1,7 @@
+# Create a program that will ask the user for their favourite colour. Have the program output a message saying something like: "Blue?! No why, that's my favourite colour too!".
+
+colour = input("what is your favourite colour?: ")
+print("Why {}.".format(colour))
+------------------------------
+
 
