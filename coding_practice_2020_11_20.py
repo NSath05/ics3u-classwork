a6e42d051a9163d9efd97b0bf3f94bf83f83f0e5
NSath05 <74628036+NSath05@users.noreply.github.com>
2020-11-20 13:22:00 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -4,4 +4,13 @@ colour = input("what is your favourite colour?: ")
 print("Why {}.".format(colour))
 ------------------------------
 
+# Create a program that asks how many cans come in a pack. Then ask the user how many packs there are. Output a message indicating the total number of cans.
+
+Cans = int(input("Please enter the number of cans: "))
+Packs = int(input("Please enter the number of packs: "))
+
+Cans = Cans * Packs
+
+print("The total number of cans is {}.".format(Cans))
+------------------------------
 
