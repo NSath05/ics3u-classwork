3cd776b68888fe13552d60cf45462a41c2d473d8
NSath05 <74628036+NSath05@users.noreply.github.com>
2020-11-27 14:11:49 -0500

    Create coding_practice_2020_11_27.py

---
+++ coding_practice_2020_11_27.py
@@ -0,0 +1,60 @@
+# Create a while loop that will print:
+    1. from -123 to 27 by 15.
+    count = -123
+    while count <= 27: 
+      print(count)
+      count +=15
+      
+    2. from 325 to 423 by 7.
+      count = 325
+    while count <= 423: 
+      print(count)
+      count +=7
+      
+    3. from -385 to 122 by 13.
+    count = -385 
+    while count <= 122: 
+      print(count)
+      count +=13
+      
+    4. from -218 to 58 by 12.
+    count = -218
+    while count <= 58: 
+      print(count)
+      count +=12
+      
+    5. from -397 to 177 by 14.
+    count = -397
+    while count <= 177: 
+      print(count)
+      count +=14
+      
+    6. from 181 to 818 by 13.
+    count = 181
+    while count <= 818: 
+      print(count)
+      count +=13
+      
+    7. from -2 to 550 by 12.
+    count = -2
+    while count <= 550: 
+      print(count)
+      count +=12
+      
+    8. from 271 to 367 by 8.
+    count = 271
+    while count <= 367: 
+      print(count)
+      count +=8
+      
+    9. from 494 to 1049 by 15.
+    count = 494
+    while count <= 1094: 
+      print(count)
+      count +=15
+      
+    10. from -223 to -135 by 2.
+    count = -223
+    while count <= -135: 
+      print(count)
+      count +=2
